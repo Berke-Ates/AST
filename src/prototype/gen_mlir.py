f0015d94be37aa218f9a8d2241e2e63878c28d58
Filip Dobrosavljevic <dofilip@student.ethz.ch>
2023-04-24 19:22:56 +0200

    Added support for if, while, yield and condition

--- src/prototype/gen_mlir.py
+++ src/prototype/gen_mlir.py
@@ -62,7 +62,7 @@ class ForLoop():
     # ssa_vars[2]: upper bound var
     # ssa_vars[3]: step var
 
-    def __init__(self, ssa_vars: List[Variable], iter_args: List[(Variable, Variable)], return_type: List[Type], index_case: bool):
+    def __init__(self, ssa_vars: List[Variable], iter_args: List[tuple[Variable, Variable]], return_type: List[Type], index_case: bool):
         if(len(ssa_vars) != 4):
             raise ValueError("SSA array requires 4 variables")
 
@@ -80,8 +80,45 @@ class ForLoop():
                     "return_type" : self.return_type, "iter_args": self.iter_args, 
                     "statements": self.statements, "index_case" : self.index_case
             }
+    
+class If():
+    def __init__(self, condition_var: Variable, return_type: List[Variable]):
+        if(condition_var.type.name != "ui1"):
+            raise ValueError("Condition variable must be 1-bit signless integer")
+        self.condition_var = condition_var
+        self.return_type = return_type
+        self.statements_then = []
+        self.statements_else = []
+        self.local_scope = []
 
+    def parse_dict(self):
+        return {"type": "if", "condition_var" : self.condition_var, "return_type": self.return_type,
+                "statements_then": self.statements_then, "statements_else": self.statements_else}
 
+class Condition():
+    def __init__(self, condition_var: Variable, args: Variable):
+        self.condition_var = condition_var
+        self.args = args # either zero or one argument
+
+    def parse_dict(self):
+        return {"type": "condition", "condition_var" : self.condition_var, "args": self.args}
+    
+class WhileDo():
+    def __init__(self, condition: Condition, assignment_list: List[tuple[Variable, Variable]], return_type: List[Variable]):
+        self.condition = condition
+        self.assignment_list = assignment_list
+        self.return_type = return_type
+        
+        # Before and after regions
+        self.statements_before = []
+        self.statements_after = []
+        self.local_scope_before = []
+        self.local_scope_after = []
+
+    def parse_dict(self):
+        return {"type": "whiledo", "condition" : self.condition, "assignment_list": self.assignment_list, "return_type": self.return_type,
+                "statements_before": self.statements_before, "statements_after": self.statements_after}
+    
 class MLIRSmith():
 
     # Contains global variables in the module
@@ -99,6 +136,8 @@ class MLIRSmith():
     t_f64 = Type("f64")
 
     t_index = Type("index")
+
+    t_ui1 = Type("ui1")
 
     # Placeholder for config files
     def __init__(self):
@@ -140,9 +179,69 @@ class MLIRSmith():
 
         # =============== Defining For Loops ===============
 
+        # Define SSA variables
+        iv = Variable("iv", self.t_i32)
+        lb = Variable("lb", self.t_i32)
+        ub = Variable("ub", self.t_i32)
+        step = Variable("step", self.t_i32)
+
+        # Define iteration variables
+        sum_iter = Variable("sum_iter", self.t_f32)
+        sum_0 = Variable("sum_0", self.t_f32)
+
+        # Initialize For loop
+        l1 = ForLoop([iv, lb, ub, step], [(sum_iter, sum_0)], [self.t_f32], False)
+
+        # Add statements and yield to loop
+        sum_next = Variable("sum_next", self.t_f32)
+
+        addf = Expression("arith.addf", [sum_iter, sum_iter], [self.t_f32])
+
+        l1.statements.append(sum_next.parse_dict(addf))
+
+        yld1 = Expression("scf.yield", [sum_next], [self.t_f32])
+
+        l1.statements.append(yld1.parse_dict())
+
+        # Add for loop to module statements
+        module.statements.append(l1.parse_dict())
 
 
+        # =============== Defining If/Else statements ===============
 
+        # Set up condition variable (type = ui1)
+        cond = Variable("cond", self.t_ui1)
+        if1 = If(cond, [self.t_f32])
+
+        yld2 = Expression("scf.yield", [sum_next], [self.t_f32])
+
+        # Add yield to then block
+        if1.statements_then.append(yld2.parse_dict())
+
+        # Add if statement to module statements
+        module.statements.append(if1.parse_dict())
+
+        # =============== Defining WhileDo loop ===============
+
+        # Set up condition for the while do loop
+        next = Variable("next", self.t_f32)
+        while_cond = Condition(cond, next)
+
+        # Set up assignment list
+        arg1 = Variable("arg1", self.t_f32)
+        init1 = Variable("init1", self.t_f32)
+
+        w1 = WhileDo(while_cond, [(arg1, init1)], [self.t_f32])
+
+        # Add 'before' region
+        addf2 = Expression("arith.addf", [arg1, arg1], [self.t_f32])
+        w1.statements_before.append(next.parse_dict(addf2))
+
+        # Add 'after' region
+        yld3 = Expression("scf.yield", [arg1], [self.t_f32])
+        w1.statements_after.append(yld3.parse_dict())
+
+        module.statements.append(w1.parse_dict())
 
         # =============== Output as MLIR ===============
         # Parse to mlir from dictionary
@@ -184,11 +283,43 @@ class MLIRSmith():
                 # Include iteration type if not index case
                 it_type = ""
                 if(not(id['index_case'])):
-                    it_type = f": {iv.type.name}"
+                    it_type = f": {ssa_vars[0].type.name}"
 
                 return f"scf.for %{iv} = %{lb} to %{ub} step %{step} " + it_type + f"{nl}" + \
                     f"iter_args({', '.join(f'%{l.name} = %{r.name}' for (l,r) in id['iter_args'])}) ->  ({', '.join(f'{r.name}' for r in id['return_type'])}) {{ {nl}" + \
                     f"{nl.join([self.parse_to_mlir(s) for s in id['statements']])} {nl} }}" 
+            
+            case "condition":
+                return f"scf.condition(%{id['condition_var'].name}) %{id['args'].name} : {id['args'].type.name}"
+            
+            case "if":
+                cond = id["condition_var"].name
+                else_output = ""
+                first_line = f"scf.if %{cond} -> ({', '.join(f'{r.name}' for r in id['return_type'])}) {{ {nl}"
+
+                # Check if else case is populated
+                if(len(id["statements_else"]) != 0):
+                    else_output = " else {{ {nl}" + \
+                    f"{nl.join([self.parse_to_mlir(s) for s in id['statements_else']])} {nl} }}" 
+
+                # Check if return type is empty, and adjust first line
+                if(len(id["return_type"]) == 0):
+                    first_line = f"scf.if %{cond} {{ {nl}"
+
+                return first_line + \
+                    f"{nl.join([self.parse_to_mlir(s) for s in id['statements_then']])} {nl} }}" + \
+                    else_output
+            
+            case "whiledo":
+                
+                assignment_list = f"({', '.join(f'%{l.name} = %{r.name}' for (l,r) in id['assignment_list'])})"
+                func_type = f": ({', '.join(f'%{l.type.name}' for (l,r) in id['assignment_list'])}) -> ({', '.join(f'{r.name}' for r in id['return_type'])})"
+
+                return "scf.while " + assignment_list + func_type + f"{{ {nl}" + \
+                    f"{nl.join([self.parse_to_mlir(s) for s in id['statements_before']])}" + \
+                    self.parse_to_mlir(id["condition"].parse_dict()) + f"{nl} }} do {{ {nl}" + \
+                    f"{nl.join([self.parse_to_mlir(s) for s in id['statements_after']])}" + \
+                    f"{nl} }}"
 
             case _:
                 return "\n"
