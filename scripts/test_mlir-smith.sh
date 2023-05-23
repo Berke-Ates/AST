ea6eae34db1d5189fbed32cde55ff56445142656
Berke Ates <beates@student.ethz.ch>
2023-05-23 17:12:31 +0200

    added testing script

---
+++ scripts/test_mlir-smith.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This script runs mlir-smith with multiple seeds and reports any crashes or
+# timeouts
+
+# Check if a path to the tool was provided.
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <path_to_mlir-smith>"
+  exit 1
+fi
+
+# The path to the tool.
+mlir_smith=$1
+
+# Check if the tool exists and is executable.
+if [ ! -x "$mlir_smith" ]; then
+  echo "Error: mlir-smith does not exist at '$mlir_smith' or is not executable."
+  exit 1
+fi
+
+# The range of seeds to test.
+start_seed=0
+end_seed=100
+
+# The timeout in seconds.
+timeout=5
+
+for ((seed = start_seed; seed <= end_seed; seed++)); do
+  if ! timeout $timeout ./"$mlir_smith" --seed $seed >/dev/null 2>&1; then
+    echo "Crash or timeout with seed: $seed"
+    exit 1
+  fi
+done
+
+echo "No crashes or timeouts found in the seed range"
+exit 0
