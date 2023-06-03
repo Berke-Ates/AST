3de9eb83e70d12c638a9fad882ea5f70b6c8eb23
Berke Ates <beates@student.ethz.ch>
2023-06-03 21:14:35 +0200

    bugfix

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -178,8 +178,8 @@ mlir-translate --mlir-to-llvmir "$mlir_dir"/"${input_name}"_ll.mlir \
   >"$mlir_dir"/"${input_name}".ll
 
 # Generate assembly
-llc -O0 "$llvm_dir"/"${input_name}".ll \
-  >"$llvm_dir"/"${input_name}".s
+llc -O0 "$mlir_dir"/"${input_name}".ll \
+  >"$mlir_dir"/"${input_name}".s
 
 # Compile & Assemble
 # shellcheck disable=SC2086
